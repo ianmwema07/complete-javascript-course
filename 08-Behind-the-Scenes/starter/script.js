816515783938cdac236dee8469b667590f7f8f3f
ian.kariuki <ianmwema07@gmail.com>
2023-12-20 16:16:43 +0300

    Worked on destructuring.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,87 @@
-'use strict';
+ 'use strict';
+//
+// console.log(addDec(1,4))
+// // console.log(addExpression(13,14))
+//
+// function addDec(a, b){
+//     a = a+b;
+//     return a;
+// }
+//
+// const addExpression = function (a, b){
+//     a = a + b;
+//     return a;
+// }
+//
+// function deleteShoppingCart(){
+//     console.log('All products deleted')
+// }
+//
+// const Jonas = {
+//     name : 'Jonas',
+//     birthYear : 1991,
+//     calcAge(){
+//         console.log(this)
+//     },
+//     greet: () => console.log(`This is the original name ${this.name}`)
+// }
+//
+// console.log('Jonas Age :: ', Jonas.calcAge())
+// console.table(Jonas)
+
+//Arrow function do not get their own this keyword.
+
+ // const calcAgeArrow = birthYear =>  {
+ //    console.log(2037 - birthYear);
+ // }
+ //
+ // calcAgeArrow(1996)
+ //
+ // console.log(this);
+ //
+ // const calcAge = function (birthYear){
+ //     console.log(2037 - birthYear)
+ // }
+
+ // Jonas.calcAge();
+ //
+ // Jonas.greet()
+
+ // let age = 31;
+ // let oldAge = age;
+ // age = 30;
+ // console.log(oldAge);
+
+ const me = {
+     name : 'Ian',
+     age : 30,
+ }
+
+ const friend = me;
+ friend.age = 27;
+
+ console.log('Me object ', me);
+ console.log('Friend object', friend);
+
+ let lastName = 'Williams';
+ let oldLastName = lastName;
+ lastName = 'Davis';
+ console.log(`Last name :: ${lastName} oldLast Name :: ${oldLastName}`)
+
+ //Copying objects
+
+ const Jessica = {
+  firstName : 'Jessica',
+  lastName : 'Williams',
+  age : 27,
+  family : ['Ely', 'Kennedy']
+ };
+
+ const JessicaCopy = Object.assign({},Jessica)
+
+ JessicaCopy.age = 35
+ JessicaCopy.lastName = 'Davis'
+ JessicaCopy.family.push('Mary')
+
+ console.table(Jessica)
+console.table(JessicaCopy)
--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -12,6 +12,22 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery:function(
+      {starterIndex = 1,
+        mainIndex = 0,
+        time = '22:00',
+        address }) {
+    console.log(`order received :: ${starterIndex} \n mainIndex is :: ${mainIndex} \n time is :: ${time} \n address is ${address}`)
+  },
+
+  orderPasta : function([ing1, ing2, ing3]){
+    console.log(`Here is your delicious pasta with ${ing1} , ${ing2}  ${ing3}`)
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -27,3 +43,64 @@ const restaurant = {
     },
   },
 };
+
+restaurant.orderDelivery({
+  time : '12:30',
+  address : 'Via del sole, 21',
+  mainIndex : 2,
+  starterIndex : 2
+})
+
+restaurant.orderDelivery({
+  time : '12:00',
+  address : 'Uthiru '
+})
+
+const arr = [7,8,9];
+const newArray = [1,2,...arr];
+console.log(`This is the new array ${newArray}`)
+
+const newMenu = [...restaurant.mainMenu,'Ugali']
+console.log(newMenu)
+const[x,y,z ] = arr;
+console.log(x, y, z)
+let [main,,secondary] = restaurant.categories;
+
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main]
+console.log(main, secondary);
+
+const [starter,mainOrder] = restaurant.order(2,0)
+console.log(starter, mainOrder)
+
+//Default values
+
+const {name : restaurantName, openingHours: hours, categories : tags} = restaurant;
+console.log(`The name is :: ${restaurantName} the opeing hours are ${hours} categories are ${tags}`)
+
+//Mutating variables
+
+//Nested Objects
+const {fri : { open : openingHour ,close :  closingHour }} = hours;
+console.log(`This is the ${openingHour} and this is the closing hour ${closingHour}`)
+
+//String
+const str = 'Mwema';
+const letters = [...str]
+console.log(letters)
+
+//Ingredients
+
+const ingredients = [
+    prompt(`Let\'s make pasta ing 1 !`),
+  prompt(`Let\'s make pasta ing 2!`),
+  prompt(`Let\'s make pasta ing 3!`)
+]
+
+console.log(ingredients)
+restaurant.orderPasta([...ingredients]);
+
+//Spread operator on objects.
+
+
